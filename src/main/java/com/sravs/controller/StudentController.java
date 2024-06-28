9903483a7440ff06c206ade2180dd157c9987052
Sravanthi Yachareni <sravanthiyachareni@gmail.com>
2024-06-28 16:06:28 +0530

    implemented Student

--- src/main/java/com/sravs/controller/StudentController.java
+++ src/main/java/com/sravs/controller/StudentController.java
@@ -5,17 +5,30 @@ import com.sravs.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class StudentController {
 
     @Autowired
     StudentService studentService;
-@PostMapping("add")
-public  Student createStudent(@RequestBody Student student){
-    return  studentService.addStudent(student);
-}
-@GetMapping("/getStudent/{id}")
-public  Student getStudentById(@PathVariable("id") int id) {
-    return  studentService.getStudent(id);
-}
+
+    @PostMapping("add")
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+
+    @GetMapping("/getStudent/{id}")
+    public Student getStudentById(@PathVariable("id") int id) {
+        return studentService.getStudent(id);
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudent() {
+        return studentService.getAll();
+    }
+    @DeleteMapping("delete")
+    public  void  deleteStudent(int id ){
+        studentService.deleteStudent(id);
+    }
 }
--- src/main/java/com/sravs/service/StudentService.java
+++ src/main/java/com/sravs/service/StudentService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Service
 public class StudentService {
 
@@ -16,5 +18,12 @@ public class StudentService {
     }
     public  Student getStudent(int id) {
         return  studentRepo.findById(id).get();
+    }
+    public List<Student> getAll() {
+        return  studentRepo.findAll();
+
+    }
+    public   void  deleteStudent(int id) {
+         studentRepo.deleteById(id);
     }
 }
